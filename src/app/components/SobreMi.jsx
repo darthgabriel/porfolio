35ec0f55643a29f32da932a5140c106242ea27be
Jose Casa <gabriel.changed@gmail.com>
2024-07-30 12:57:01 -0400

    error en profesion

--- src/app/components/SobreMi.jsx
+++ src/app/components/SobreMi.jsx
@@ -3,11 +3,11 @@ export default function SobreMi () {
     <section id='sobremi'>
       <h4 className='text-light fw-bold'>
         <i className='bi bi-code-slash' />
-        {' '}Sobre Mi
+        {' '}Sobre Mí
       </h4>
       <div className='text-light mt-2 bg-dark border border-light p-2'>
         <p>
-          Soy un Ingeniero de Sistemas con mas de 10 años de experiencia en el desarrollo de software.
+          Soy un Ingeniero de Computación con mas de 10 años de experiencia en el desarrollo de software.
         </p>
         <p>
           He trabajado en diferentes areas de la informatica, desde soporte tecnico hasta desarrollo de aplicaciones web y moviles.
